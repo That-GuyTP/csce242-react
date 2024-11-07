998a4d6a396615d02d298dde9c1d2895bdd61bdf
Thomas Peterson <tap6@email.sc.edu>
2024-11-07 10:01:12 -0500

    Adding stuffs

---
+++ react-01/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return <div id="temp">
+    <h1>Hello World</h1>
+    <p>I love coding. I'm enjoying learning all about HTML, CSS, JS, and more!</p>
+    <p>This is the react based js.</p>
+  </div>;
+}
+
+export default App;
